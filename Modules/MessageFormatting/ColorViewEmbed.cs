af8878029dd83c527418eb8b441c823af201c81c
Kyle <piper2200s@gmail.com>
2021-09-07 15:37:24 -0500

    initial Color Module
    Color group exists.
    Admire command will show color

---
+++ Modules/MessageFormatting/ColorViewEmbed.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UsefulDiscordBot.Modules.MessageFormatting
+{
+  public class ColorViewEmbed : EmbedBuilder
+  {
+    public ColorViewEmbed( string colorHex )
+    {
+      Title = "Admire this color...";
+      Color = new Discord.Color((uint)System.Convert.ToInt32(colorHex, 16));
+    }
+
+  }
+}
